d043a84f8c38f98da5aa6153ffd9311131c969c2
Prabhu Patil <prapatil@riverbed.com>
2017-09-01 14:27:09 +0530

    Final CI changes

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -15,7 +15,7 @@ class TestDevelopmentConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
-        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == 'postgres://postgres:postgres@users-db:5432/users_test')
+        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == 'postgres://postgres:postgres@users-db:5432/users_dev')
 
 class TestTestingConfig(TestCase):
     def create_app(self):
@@ -27,11 +27,7 @@ class TestTestingConfig(TestCase):
         self.assertTrue(app.config['DEBUG'])
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
-        self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_test'
-        )
-
+      
 
 class TestProductionConfig(TestCase):
     def create_app(self):
